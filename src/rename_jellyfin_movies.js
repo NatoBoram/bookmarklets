8c0a373f72ed75a90a19913841c3043bc16dc350
Nato Boram <NatoBoram@users.noreply.github.com>
2026-01-03 14:50:28 -0500

    ✨ Rename movies on Jellyfin (#13)
    * ✨ Rename movies on Jellyfin
    
    * 👔 Copy file name to title
    
    * 💡 Change example names
    
    * 🚨 "use strict"
    
    * 💡 No inline comment
    
    * 🦺 Throw when enabledName is not found
    
    * ♻️ .values().toArray()
    
    * ⬆️ pnpm update

---
+++ src/rename_jellyfin_movies.js
@@ -0,0 +1,110 @@
+(async () => {
+	"use strict";
+
+	async function cancel() {
+		console.log("Cancelling...");
+		const btnCancel = document.querySelector(".btnCancel");
+		if (!btnCancel)
+			throw new Error("Cancel button not found", { cause: btnCancel });
+		btnCancel.click();
+		await new Promise(r => setTimeout(r, 1000));
+	}
+
+	async function save() {
+		console.log("Saving...");
+		const btnSave = document.querySelector(".btnSave");
+		if (!btnSave) throw new Error("Save button not found", { cause: btnSave });
+		btnSave.click();
+		await new Promise(r => setTimeout(r, 1000));
+	}
+
+	const movies = document
+		.querySelectorAll(".card.portraitCard.card-hoverable.card-withuserdata")
+		.values()
+		.toArray();
+
+	for (let index = 0; index < movies.length; index++) {
+		console.log(`Renaming movie ${index}/${movies.length}...`);
+
+		const movie = document.querySelector(
+			`.card.portraitCard.card-hoverable.card-withuserdata[data-index="${index}"]`,
+		);
+		if (!movie) throw new Error("Movie not found", { cause: { movie, index } });
+
+		console.log("Opening the context menu");
+		movie.dispatchEvent(new MouseEvent("contextmenu", { bubbles: true }));
+		await new Promise(r => setTimeout(r, 1000));
+
+		const editMetadata = document
+			.querySelectorAll(".listItemBodyText.actionSheetItemText")
+			.values()
+			.toArray()
+			.find(b => b.innerText === "Edit metadata");
+
+		if (!editMetadata)
+			throw new Error("Edit metadata button not found", {
+				cause: editMetadata,
+			});
+
+		editMetadata.click();
+		await new Promise(r => setTimeout(r, 1000));
+
+		/** @type {HTMLInputElement | null} */
+		const txtPath = document.getElementById("txtPath");
+		if (!txtPath) throw new Error("Path input not found", { cause: txtPath });
+
+		/** @type {HTMLInputElement | null} */
+		const txtName = document.getElementById("txtName");
+		if (!txtName) throw new Error("Title input not found", { cause: txtName });
+
+		/* Extract movie name from txtPath. The value looks like
+		"/syncthing/Films/Movie Name - VOA_VFQ_VFF.mkv" and I need
+		"Movie Name - VOA, VFQ, VFF". */
+		const splitSlash = txtPath.value.split("/");
+
+		/** @example "Movie Name - VOA_VFQ_VFF.mkv" */
+		const afterSlash = splitSlash[splitSlash.length - 1];
+		const byDots = afterSlash.split(".");
+
+		/** @example "Movie Name - VOA_VFQ_VFF" */
+		const beforeDot = byDots.slice(0, byDots.length - 1).join(".");
+		const splitDash = beforeDot.split(" - ");
+
+		/** @example "VOA_VFQ_VFF" */
+		const afterDash = splitDash[splitDash.length - 1];
+
+		/** @example "VOA, VFQ, VFF" */
+		const languages = afterDash.replaceAll("_", ", ");
+
+		/** @example "Movie Name - VOA, VFQ, VFF" */
+		const movieName = beforeDot.replace(afterDash, languages);
+
+		if (txtName.value == movieName) {
+			console.warn(`Skipping "${txtName.value}"`);
+			await cancel();
+			continue;
+		}
+
+		console.info(`Renaming "${txtName.value}" to "${movieName}"...`);
+		txtName.value = movieName;
+
+		/** @type {HTMLInputElement | null} */
+		const enabledName = document.querySelector('input[data-value="Name"]');
+		if (!enabledName)
+			throw new Error("Enabled Name checkbox not found", {
+				cause: enabledName,
+			});
+
+		if (enabledName.checked) {
+			console.log("Disabling Enabled Name");
+			enabledName.click();
+			await new Promise(r => setTimeout(r, 1000));
+		} else {
+			console.warn("Enabled Name is already disabled");
+		}
+
+		await save();
+	}
+
+	console.log("All movies renamed.");
+})();
